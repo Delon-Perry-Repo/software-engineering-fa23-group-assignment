2c05e296725fa952877eb250e2451602a93aa932
Zach <sierrazja@gmail.com>
2023-10-31 13:49:28 -0500

    ADDED: Files to connect to the database using NodeJS along with authentication methods

---
+++ scripts/database.js
@@ -0,0 +1,33 @@
+const mysql = require('mysql2');
+const fs = require('fs');
+const { host, user, password, database, ca} = require('./config');
+
+// dirname is the path to your .pem key
+const dirname = 'C:/Users/sierr/OneDrive/Desktop/COMPSCI/software-engineering-fa23-group-assignment/'
+console.log("user : " + user);
+
+const connection = mysql.createConnection({
+    host,
+    user,
+    password,
+    database,
+    ssl : {
+        ca: fs.readFileSync( dirname + ca),
+    }
+});
+
+connection.connect((error) => {
+    if (error){
+        console.log("Error : " + error);
+        return 1;
+    }
+});
+
+connection.query("select * from user", (err, res) =>{
+    if (err){
+        console.log(err);
+    }
+    console.log(res);
+});
+
+connection.end();
