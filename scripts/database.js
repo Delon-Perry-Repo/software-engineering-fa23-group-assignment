3a330b24829bb1f4d8fd9c5b19193d9c9635457a
Zach <sierrazja@gmail.com>
2023-10-31 13:51:35 -0500

    Fixed pathway for database.js

--- scripts/database.js
+++ scripts/database.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const { host, user, password, database, ca} = require('./config');
 
 // dirname is the path to your .pem key
-const dirname = 'C:/Users/sierr/OneDrive/Desktop/COMPSCI/software-engineering-fa23-group-assignment/'
+const dirname = 'C:/Users/sierr/OneDrive/Desktop/COMPSCI/SWE PROJECT/software-engineering-fa23-group-assignment/'
 console.log("user : " + user);
 
 const connection = mysql.createConnection({
