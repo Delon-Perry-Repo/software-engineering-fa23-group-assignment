8962b36650760c1747d6429833bdee293946c085
Zach <sierrazja@gmail.com>
2023-11-01 18:23:50 -0500

    Fixed some issues

--- scripts/database.js
+++ scripts/database.js
@@ -4,7 +4,6 @@ const { host, user, password, database, ca} = require('./config');
 
 // dirname is the path to your .pem key
 const dirname = 'C:/Users/sierr/OneDrive/Desktop/COMPSCI/SWE PROJECT/software-engineering-fa23-group-assignment/'
-console.log("user : " + user);
 
 const connection = mysql.createConnection({
     host,
