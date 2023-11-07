3750e4b30c07764c37b402d8d22c936ada229ec4
Delon-Perry-Repo <114537428+Delon-Perry-Repo@users.noreply.github.com>
2023-11-07 14:14:45 -0600

    Connected to Database

--- scripts/validation.js
+++ scripts/validation.js
@@ -1,76 +1,109 @@
-// JavaScript Document
-function validateEmail(email){
-	var validRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-	var elEmailStatus = document.getElementById('emailStatus');
-	elEmailStatus.classList.add("alert");
-	if (email.value.match(validRegex)) {
-		elEmailStatus.classList.remove("alert-danger");
-		elEmailStatus.classList.add("alert-success");
-		elEmailStatus.innerHTML = "Email is valid!";
-	}
-	else {elEmailStatus.classList.remove("alert-sucess");
-		elEmailStatus.classList.add("alert-danger");
-		elEmailStatus.innerHTML = "Email is not valid!";
-	}
-}
+function checkFieldEmail(textField) {
 
-function validateName(name, statusElement) {
-  var validRegex = /^[a-zA-Z-' ]*$/;
-  statusElement.classList.add("alert");
-  if (name.value.match(validRegex)) {
-    statusElement.classList.remove("alert-danger");
-    statusElement.classList.add("alert-success");
-    statusElement.innerHTML = "Name is valid!";
+  var validRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  var elEmailStatus = document.getElementById("emailStatus");
+  elEmailStatus.classList.add("alert");
+
+  if ((textField.value != null) && validRegex.test(textField.value)) {
+    elEmailStatus.classList.remove("alert-failure");
+    elEmailStatus.classList.add("alert-success");
+    elEmailStatus.innerHTML = "Email Address Is Valid!";
+    return true;
   } else {
-    statusElement.classList.remove("alert-success");
-    statusElement.classList.add("alert-danger");
-    statusElement.innerHTML = "Name is not valid!";
+    elEmailStatus.classList.remove("alert-success");
+    elEmailStatus.classList.add("alert-failure");
+    elEmailStatus.innerHTML = "Email Address Is Invalid!";
+    return false;
   }
 }
 
-function validatePhone(phone, statusElement) {
-  var validRegex = /^[0-9]{10}$/;
-  statusElement.classList.add("alert");
-  if (phone.value.match(validRegex)) {
-    statusElement.classList.remove("alert-danger");
-    statusElement.classList.add("alert-success");
-    statusElement.innerHTML = "Phone number is valid!";
+function checkFieldUserName(textField) {
+
+  var validRegex = /^(?=[a-zA-Z0-9._]{8,20}$)(?!.*[_.]{2})[^_.].*[^_.]$/;
+  var elUserStatus = document.getElementById("userStatus");
+  elUserStatus.classList.add("alert");
+
+  if ((textField.value != null) && validRegex.test(textField.value)) {
+    elUserStatus.classList.remove("alert-failure");
+    elUserStatus.classList.add("alert-success");
+    elUserStatus.innerHTML = "Username Is Valid!";
+    return true;
   } else {
-    statusElement.classList.remove("alert-success");
-    statusElement.classList.add("alert-danger");
-    statusElement.innerHTML = "Phone number is not valid!";
+    elUserStatus.classList.remove("alert-success");
+    elUserStatus.classList.add("alert-failure");
+    elUserStatus.innerHTML = "Username Is Invalid - Only Alphanumeric or Underscores Allowed (Min: 8 Max: 20)!";
+    return false;
   }
 }
 
-function validateComments(comments, statusElement) {
-  statusElement.classList.add("alert");
-  if (comments.value.trim() !== "") {
-    statusElement.classList.remove("alert-danger");
-    statusElement.classList.add("alert-success");
-    statusElement.innerHTML = "Comments are valid!";
+function checkFieldPassword(textField) {
+
+  var validRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,20}$/;
+  var elPassStatus = document.getElementById("passStatus");
+  elPassStatus.classList.add("alert");
+
+  if ((textField.value != null) && validRegex.test(textField.value)) {
+    elPassStatus.classList.remove("alert-failure");
+    elPassStatus.classList.add("alert-success");
+    elPassStatus.innerHTML = "Password Is Valid!";
+    return true;
   } else {
-    statusElement.classList.remove("alert-success");
-    statusElement.classList.add("alert-danger");
-    statusElement.innerHTML = "Comments cannot be empty!";
+    if (textField.value == "") {
+      elPassStatus.classList.remove("alert-success");
+      elPassStatus.classList.add("alert-failure");
+      elPassStatus.innerHTML = "Password Can Not Be Left Blank!";
+      return false;
+    }
+    elPassStatus.classList.remove("alert-success");
+    elPassStatus.classList.add("alert-failure");
+    elPassStatus.innerHTML = "Password Is Invalid - Minimum Eight And Maximum 20 Characters, At Least One Uppercase Letter, One Lowercase Letter, One Number, And One Special Character!";
+    return false;
   }
 }
 
+function confirmPassword(firstInput, secondInput) {
 
-var elEmail = document.getElementById('email');
-elEmail.addEventListener('blur', function() {validateEmail(elEmail);},false);
+  var elPassStatusConf = document.getElementById("confPassStatus");
+  elPassStatusConf.classList.add("alert");
 
-var elFirstName = document.getElementById('firstName');
-var elFirstNameStatus = document.getElementById('firstNameStatus');
-elFirstName.addEventListener('blur', function() {validateName(elFirstName, elFirstNameStatus);}, false);
+  if (firstInput.value === secondInput.value) {
+    if (secondInput.value == "") {
+      elPassStatusConf.classList.remove("alert-success");
+      elPassStatusConf.classList.add("alert-failure");
+      elPassStatusConf.innerHTML = "Password Can Not Be Left Blank!";
+      return false;
+    }
+    elPassStatusConf.classList.remove("alert-failure");
+    elPassStatusConf.classList.add("alert-success");
+    elPassStatusConf.innerHTML = "Passwords Match!";
+    return true;
+  } else {
+    elPassStatusConf.classList.remove("alert-success");
+    elPassStatusConf.classList.add("alert-failure");
+    elPassStatusConf.innerHTML = "Passwords Don't Match!";
+    return false;
+  }
 
-var elLastName = document.getElementById('lastName');
-var elLastNameStatus = document.getElementById('lastNameStatus');
-elLastName.addEventListener('blur', function() {validateName(elLastName, elLastNameStatus);}, false);
+}
 
-var elPhone = document.getElementById('phone');
-var elPhoneStatus = document.getElementById('phoneStatus');
-elPhone.addEventListener('blur', function() {validatePhone(elPhone, elPhoneStatus);}, false);
+var userEmail = document.getElementById('newemail');
+var userName = document.getElementById('newuname');
+var userPassword = document.getElementById('newpsw');
+var userPasswordConf = document.getElementById('confirmnewpsw');
 
-var elComments = document.getElementById('comments');
-var elCommentsStatus = document.getElementById('commentsStatus');
-elComments.addEventListener('blur', function() {validateComments(elComments, elCommentsStatus);}, false);
+userEmail.addEventListener('blur', function () {
+  checkFieldEmail(userEmail);
+}, false);
+
+userName.addEventListener('blur', function () {
+  checkFieldUserName(userName);
+}, false);
+
+userPassword.addEventListener('blur', function () {
+  checkFieldPassword(userPassword);
+}, false);
+
+userPasswordConf.addEventListener('blur', function () {
+  confirmPassword(userPassword, userPasswordConf);
+}, false);
+
