821f78a945cfd92c0316b833928c8f445a27c266
SmallManGuy <100001707+SmallManGuy@users.noreply.github.com>
2023-11-20 10:21:51 -0600

    Add files via upload

---
+++ scripts/shopScripts.js
@@ -0,0 +1,47 @@
+const products = [
+{
+    id: 1,
+    name: "T-shirt 1",
+    price: 29.99,
+    instock: 100,
+    description:
+        "T shirt pretty cool",
+    imgSrc: "res/shirt_001.png",
+},
+{
+    id: 2,
+    name: "T-shirt 2",
+    price: 29.99,
+    instock: 100,
+    description:
+        "T shirt pretty cool",
+    imgSrc: "res/shirt_002.png",
+},
+{
+    id: 3,
+    name: "T-shirt 3",
+    price: 29.99,
+    instock: 100,
+    description:
+        "T shirt pretty cool",
+    imgSrc: "res/shirt_003.png",
+},
+{
+    id: 4,
+    name: "T-shirt 4",
+    price: 29.99,
+    instock: 100,
+    description:
+        "T shirt pretty cool",
+    imgSrc: "res/shirt_004.png",
+},
+{
+    id: 5,
+    name: "T-shirt 5",
+    price: 29.99,
+    instock: 100,
+    description:
+        "T shirt pretty cool",
+    imgSrc: "res/shirt_005.png",
+}
+];
