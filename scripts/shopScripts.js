0763449355fce32458fc0e5af9ca7405cd296257
Zach <sierrazja@gmail.com>
2023-11-21 12:44:46 -0600

    fixed

--- scripts/shopScripts.js
+++ scripts/shopScripts.js
@@ -5,7 +5,7 @@ const products = [
     price: 99.99,
     instock: 5,
     description:
-        "T shirt pretty cool",
+        "Made with real minecraft diamonds",
     imgSrc: "res/shirt_001.png",
 },
 {
@@ -14,16 +14,16 @@ const products = [
     price: 59.99,
     instock: 10,
     description:
-        "T shirt pretty cool",
+        "I remember the good old days",
     imgSrc: "res/shirt_002.png",
 },
 {
     id: 3,
-    name: "Any",
+    name: "King",
     price: 29.99,
     instock: 12,
     description:
-        "T shirt pretty cool",
+        "The King of NY Returns!",
     imgSrc: "res/shirt_003.png",
 },
 {
@@ -32,7 +32,7 @@ const products = [
     price: 19.99,
     instock: 100,
     description:
-        "T shirt pretty cool",
+        "YUM!",
     imgSrc: "res/shirt_004.png",
 },
 {
@@ -41,7 +41,25 @@ const products = [
     price: 79.99,
     instock: 8,
     description:
-        "T shirt pretty cool",
+        "Air drop!",
     imgSrc: "res/shirt_005.png",
+},
+{
+    id: 6,
+    name: "Vote for Pedro",
+    price: 39.99,
+    instock: 50,
+    description:
+        "Insert Movie Quote",
+    imgSrc: "res/shirt_011.png",
+},
+{
+    id: 7,
+    name: "Any",
+    price: 9.99,
+    instock: 2,
+    description:
+        "Idk it's a shirt",
+    imgSrc: "res/shirt_015.png",
 }
 ];
