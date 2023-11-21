af034507e58e7404236882ab97c46c1c413a8f93
SmallManGuy <100001707+SmallManGuy@users.noreply.github.com>
2023-11-21 12:42:57 -0600

    Add files via upload

---
+++ shopScripts.js
@@ -0,0 +1,65 @@
+const products = [
+{
+    id: 1,
+    name: "Diamond",
+    price: 99.99,
+    instock: 5,
+    description:
+        "Made with real minecraft diamonds",
+    imgSrc: "res/shirt_001.png",
+},
+{
+    id: 2,
+    name: "Led",
+    price: 59.99,
+    instock: 10,
+    description:
+        "I remember the good old days",
+    imgSrc: "res/shirt_002.png",
+},
+{
+    id: 3,
+    name: "King",
+    price: 29.99,
+    instock: 12,
+    description:
+        "The King of NY Returns!",
+    imgSrc: "res/shirt_003.png",
+},
+{
+    id: 4,
+    name: "Rolling Stones",
+    price: 19.99,
+    instock: 100,
+    description:
+        "YUM!",
+    imgSrc: "res/shirt_004.png",
+},
+{
+    id: 5,
+    name: "Plane",
+    price: 79.99,
+    instock: 8,
+    description:
+        "Air drop!",
+    imgSrc: "res/shirt_005.png",
+},
+{
+    id: 6,
+    name: "Vote for Pedro",
+    price: 39.99,
+    instock: 50,
+    description:
+        "Insert Movie Quote",
+    imgSrc: "res/shirt_011.png",
+},
+{
+    id: 7,
+    name: "Any",
+    price: 9.99,
+    instock: 2,
+    description:
+        "Idk it's a shirt",
+    imgSrc: "res/shirt_015.png",
+}
+];
